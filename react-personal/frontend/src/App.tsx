d1ca15438618698eb95078104d6c8095b91a3075
Abhimanyu Verma <abhimanyuverma2001@gmail.com>
2025-11-04 21:55:09 -0700

    updated

--- react-personal/frontend/src/App.tsx
+++ react-personal/frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Hero from "./components/Hero";
 import TechScroller from "./components/TechScroller";
 
